c3aa95a2a10120da9b0b56bdbd2bd35d9b4a6d7f
Lourdu Radjou <rajlourdu15@gmail.com>
2024-08-23 13:04:22 +0530

    Installed framer motion

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,13 +26,6 @@ export default {
 				'white-400': 'rgba(255, 255, 255, 0.80)',
 				'primary-yellow': '#fdc500',
 			},
-			boxShadow: {
-				'3xl': '0 10px 40px rgba(0, 0, 0, 0.1)',
-			},
-			backgroundImage: {
-				hero: "url('assets/images/collection-background.svg')",
-				card: "url('assets/images/thumbnail-background.svg')",
-			},
 			screens: {
 				wide: '1440px',
 			},
